743fe36534eda0d947495c1bba22bc801210e38b
edogola4 <edogola4@gmail.com>
2025-02-24 12:16:43 +0300

    all test passsing

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,26 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+    """
+Add an instance method "sit" that will print "The dog is sitting"
+"""
+    def sit(self):
+        print("The dog is sitting")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
+
+# fido.sit()
+
+# bark()
+
+fido.sit()
